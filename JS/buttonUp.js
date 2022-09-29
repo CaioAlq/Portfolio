1e67221fef6d95c30144bd33d84e7dc8b6583355
CaioAlq <caioalqdev@gmail.com>
2022-09-28 23:46:24 -0300

    Fix buttonUp.js

--- JS/buttonUp.js
+++ JS/buttonUp.js
@@ -1,12 +1,14 @@
-let mybutton = document.getElementById("myBtn");
+let myButton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+    scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    myButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = "none";
   }
 }
 
